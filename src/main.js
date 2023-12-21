fbc2d396d9770dbe1071b464bc7e292863564830
Cosmeak <guillaume.fine@cosmeak.dev>
2023-12-21 21:50:55 +0100

    feat: add amplify to vue

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Amplify } from 'aws-amplify'
+import config from './amplifyconfiguration.json'
 
 const app = createApp(App)
+Amplify.configure(config);
 
 app.use(router)
 
---
+++ src/views/NasaView.vue
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<template>
+	La nasa la
+</template>
