f979cae793c8863187c50f250b02dfea17f1bc78
cosmeak <guillaume.fine@cosmeak.dev>
2023-12-22 13:58:38 +0100

    feat: add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,ts,svelte}'],
 	theme: {
+		colors: {
+			black: '#333333',
+			white: '#FFFFFF',
+			lightgray: '#F8F8F8',
+			blue: '#2541B2',
+			yellow: '#F9DB6D',
+			green: '#73AB84',
+			red: '#FF3A3A'
+		},
 		extend: {}
 	},
 	plugins: []
