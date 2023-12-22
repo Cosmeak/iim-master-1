633a72e6198b26d0d0f0446c2094345d1fc1b450
cosmeak <guillaume.fine@cosmeak.dev>
2023-12-22 16:34:33 +0100

    feat(component): basic button

---
+++ src/lib/Components/Ibutton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+export let text: string = "Button";
+export let color: string = "green";
+
+const colors = {
+	"black": "bg-black text-white hover:bg-black-dark",
+	"white": "bg-white text-black hover:bg-white-dark",
+	"lightgray": "bg-lightgray hover:bg-lightgray-dark",
+	"blue": "bg-blue text-white hover:bg-blue-dark",
+	"yellow": "bg-yellow hover:bg-yellow-dark",
+	"green": "bg-green text-white hover:bg-green-dark",
+	"red": "bg-red hover:bg-red-dark"
+};
+</script>
+
+<button 
+	type={$$props.type}
+	class={$$props.class + ` shadow-sm px-4 py-2 rounded duration-200 ` + colors[color] }
+>
+	{text}
+</button>
