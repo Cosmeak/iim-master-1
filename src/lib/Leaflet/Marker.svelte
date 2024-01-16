2801ba600ce593a32b2a86b2541b34625c472d24
cosmeak <guillaume.fine@cosmeak.dev>
2024-01-16 23:45:56 +0100

    feat(leaflet): marker component

---
+++ src/lib/Leaflet/Marker.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { onMount, onDestroy, getContext, setContext } from 'svelte';
+  import Leaflet from "leaflet";
+
+  export let width: number;
+  export let height: number;
+  export let latLng: Leaflet.LatLngExpression;
+
+  let marker: Leaflet.Marker | undefined;
+  let container: HTMLElement;
+
+  // Get map from parent
+  const { getMap }: { getMap: () => Leaflet.Map | undefined } = getContext('map');
+  const map = getMap();
+
+  setContext("layer", {
+    // Leaflet Marker is a child class from Leaflet Layer
+    getLayer: () => marker,
+  });
+
+  onMount(() => {
+    if (map) {
+      const icon = Leafet.divIcon({
+        html: container,
+        className: "map-marker",
+        iconSize: Leaflet.point(width, height),
+      });
+      marker = Leaflet.marker(latLng).addTo(map);
+    }
+  });
+
+  onDestroy(() => {
+    // Clear marker from client
+    marker?.remove();
+    marker = undefined;
+  })
+</script>
+
+<div bind:this={container}>
+  <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import type { LatLngExpression } from "leaflet";
+import Map from "$lib/Leaflet/Map.svelte";
+
+const initialView: LatLngExpression = [48.890577, 2.241656];
+const initialZoom: number = 13;
+</script>
+
+<div class="w-full h-screen">
+  <Map view={initialView} zoom={initialZoom} />
+</div>
