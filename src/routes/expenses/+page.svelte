9dd3851c3d331846a7cda9818de3367a723a5837
cosmeak <guillaume.fine@cosmeak.dev>
2024-01-17 23:40:03 +0100

    feat(expenses): first section

---
+++ src/routes/expenses/+page.svelte
@@ -0,0 +1,32 @@
+<section class="w-full h-screen bg-gradient-to-b from-white from-10% to-[#9DCDE4] flex justify-center items-center relative">
+  <div class="w-7/12 flex flex-col gap-32">
+    <h4 class="text-blue text-4xl font-semibold text-center">Dépenses de fonctionnement d'Issy-les-Moulineaux par types de services à la population</h4>
+
+    <div class="flex justify-between">
+      <div class="px-12 py-16 rounded-lg text-white bg-[#8BCBEA] text-5xl text-center">
+        <p>En 2021</p>
+        <p class="pt-8">31M €</p>
+      </div>
+
+      <div class="px-12 py-16 rounded-lg text-white bg-blue text-5xl text-center">
+        <p>En 2022</p>
+        <p class="pt-8">31M €</p>
+      </div>
+
+      <div class="px-12 py-16 rounded-lg text-white bg-blue-dark text-5xl text-center">
+        <p>En 2023</p>
+        <p class="pt-8">31M €</p>
+      </div>
+    </div>
+
+    <p class="text-2xl text-blue">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium blandit consectetur. Fusce sed accumsan metus, vitae fringilla augue. Duis tristique ligula vitae ante lacinia porta. Aliquam volutpat at nulla ut posuere. Vivamus semper eros et mauris tempor, vel pretium mauris commodo. Sed luctus sem lacus, a feugiat massa tempus ac. Pellentesque tempus felis eu nisl condimentum, eget ornare mauris bibendum. Maecenas cursus dui id cursus gravida. Donec ante ex, vulputate a nunc vitae, sagittis ornare turpis. Donec viverra purus sed feugiat ornare. Duis id metus id quam rutrum laoreet eu at est.
+    </p>
+  </div>
+  
+  <img src="assets/expenses-hero.svg" class="absolute -left-44 -translate-y-1/2 top-1/3 h-10/12">
+</section>
+
+<section>
+  
+</section>
