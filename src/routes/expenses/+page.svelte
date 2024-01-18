579179666b6de9443dcb9f7dd1cd77e8e8856a14
cosmeak <guillaume.fine@cosmeak.dev>
2024-01-18 22:01:12 +0100

    fix(expenses): text color

--- src/routes/expenses/+page.svelte
+++ src/routes/expenses/+page.svelte
@@ -18,7 +18,7 @@
 
 <section class="w-full h-screen bg-gradient-to-b from-white from-10% to-[#9DCDE4] flex justify-center items-center relative">
   <div class="w-7/12 flex flex-col gap-32">
-    <h2 class="text-blue text-4xl font-semibold text-center">Dépenses de fonctionnement d'Issy-les-Moulineaux par types de services à la population</h2>
+    <h2 class="text-blue-dark text-4xl font-semibold text-center">Dépenses de fonctionnement d'Issy-les-Moulineaux par types de services à la population</h2>
 
     <div class="flex justify-between">
       <div class="px-12 py-16 rounded-lg text-white bg-[#8BCBEA] text-5xl text-center">
@@ -37,7 +37,7 @@
       </div>
     </div>
 
-    <p class="text-2xl text-blue">
+    <p class="text-2xl text-blue-dark">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium blandit consectetur. Fusce sed accumsan metus, vitae fringilla augue. Duis tristique ligula vitae ante lacinia porta. Aliquam volutpat at nulla ut posuere. Vivamus semper eros et mauris tempor, vel pretium mauris commodo. Sed luctus sem lacus, a feugiat massa tempus ac. Pellentesque tempus felis eu nisl condimentum, eget ornare mauris bibendum. Maecenas cursus dui id cursus gravida. Donec ante ex, vulputate a nunc vitae, sagittis ornare turpis. Donec viverra purus sed feugiat ornare. Duis id metus id quam rutrum laoreet eu at est.
     </p>
   </div>
@@ -55,7 +55,7 @@
   </div>
 </section>
 
-<section class="text-blue p-24 flex gap-8">
+<section class="text-blue-dark p-24 flex gap-8">
   <div>
     <h2 class="text-3xl font-bold">Services utilisés</h2>
     <p class="pt-8 text-xl">
