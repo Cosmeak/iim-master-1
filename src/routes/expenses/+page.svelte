4dbd262355e179879f2fdd877426db3bde1f7004
cosmeak <guillaume.fine@cosmeak.dev>
2024-02-13 12:23:41 +0100

    feat: expenses chart data from api

--- src/routes/expenses/+page.svelte
+++ src/routes/expenses/+page.svelte
@@ -1,71 +1,117 @@
 <script lang="ts">
-  import ApexCharts from "$lib/ApexCharts.svelte";
+	import ApexCharts from '$lib/ApexCharts.svelte';
+	import groupBy from '$lib/Utils/groupBy.js';
 
-  const chartOptions = {
-    legend: { show: false },
-    chart: {
-      toolbar: { show: false },
-    },
-    xaxis: {
-      categories: [2008,2009,2009,2010,2011,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023]
-    }
-  };
-  const chartSeries: Array<any> = [{
-    serie: 'Dépenses par année',
-    data: [52000,54000,58000,61000,63000,67000,70000,74000,77000,79000,81000,85000,89000,95000,101000,110000],
-  }]
+	export let data; // data from api
+
+	const groupedData: Array<Array<Object>> = groupBy(
+		data.data,
+		'depenses_reelles_services_population'
+	);
+
+	console.log(groupedData);
+
+	console.log(data);
+
+	const chartOptions = {
+		chart: {
+			toolbar: { show: false }
+		},
+		xaxis: {
+			categories: [...new Set(data.data.map((item) => item.exercice))].reverse()
+		}
+	};
+
+	let chartSeries: Array<Object> = [];
+	for (const [key, value] of Object.entries(groupedData)) {
+		chartSeries.push({
+			name: key,
+			data: value.map((item) => item.montant_en_euros).reverse()
+		});
+	}
 </script>
 
-<section class="w-full h-screen bg-gradient-to-b from-white from-10% to-[#9DCDE4] flex justify-center items-center relative">
-  <div class="w-7/12 flex flex-col gap-32">
-    <h2 class="text-blue-dark text-4xl font-semibold text-center">Dépenses de fonctionnement d'Issy-les-Moulineaux par types de services à la population</h2>
+<svelte:head>
+	<title>
+		Dépenses de fonctionnement d'Issy-les-Moulineaux par types de services à la population
+	</title>
+</svelte:head>
 
-    <div class="flex justify-between">
-      <div class="px-12 py-16 rounded-lg text-white bg-[#8BCBEA] text-5xl text-center">
-        <p>En 2021</p>
-        <p class="pt-8">31M €</p>
-      </div>
+<section
+	class="w-full h-screen bg-gradient-to-b from-white from-10% to-[#9DCDE4] flex justify-center items-center relative"
+>
+	<div class="w-7/12 flex flex-col gap-32">
+		<h2 class="text-blue-dark text-4xl font-semibold text-center">
+			Dépenses de fonctionnement d'Issy-les-Moulineaux par types de services à la population
+		</h2>
 
-      <div class="px-12 py-16 rounded-lg text-white bg-blue text-5xl text-center">
-        <p>En 2022</p>
-        <p class="pt-8">31M €</p>
-      </div>
+		<div class="flex justify-between">
+			<div class="px-12 py-16 rounded-lg text-white bg-[#8BCBEA] text-5xl text-center">
+				<p>En 2021</p>
+				<p class="pt-8">31M €</p>
+			</div>
 
-      <div class="px-12 py-16 rounded-lg text-white bg-blue-dark text-5xl text-center">
-        <p>En 2023</p>
-        <p class="pt-8">31M €</p>
-      </div>
-    </div>
+			<div class="px-12 py-16 rounded-lg text-white bg-blue text-5xl text-center">
+				<p>En 2022</p>
+				<p class="pt-8">31M €</p>
+			</div>
 
-    <p class="text-2xl text-blue-dark">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium blandit consectetur. Fusce sed accumsan metus, vitae fringilla augue. Duis tristique ligula vitae ante lacinia porta. Aliquam volutpat at nulla ut posuere. Vivamus semper eros et mauris tempor, vel pretium mauris commodo. Sed luctus sem lacus, a feugiat massa tempus ac. Pellentesque tempus felis eu nisl condimentum, eget ornare mauris bibendum. Maecenas cursus dui id cursus gravida. Donec ante ex, vulputate a nunc vitae, sagittis ornare turpis. Donec viverra purus sed feugiat ornare. Duis id metus id quam rutrum laoreet eu at est.
-    </p>
-  </div>
-  
-  <img src="./assets/expenses-hero.svg" class="absolute -left-44 -translate-y-1/2 top-1/3 h-10/12" alt="">
+			<div class="px-12 py-16 rounded-lg text-white bg-blue-dark text-5xl text-center">
+				<p>En 2023</p>
+				<p class="pt-8">31M €</p>
+			</div>
+		</div>
+
+		<p class="text-2xl text-blue-dark">
+			Explorez les détails financiers avec une perspective approfondie sur la répartition des
+			dépenses de fonctionnement d’Issy-les-Moulineaux avec le Fonds de Solidarité des communes de
+			la Région Île-de-France (FSRIF) sur la période allant de 2008 à 2020. Vous pouvez avoir un
+			détail des dépenses en fonction de leur type afin de comprendre les tendances, les évolutions
+			et les dynamiques budgétaires au fil du temps.
+		</p>
+	</div>
+
+	<img
+		src="./assets/expenses-hero.svg"
+		class="absolute -left-44 -translate-y-1/2 top-1/3 h-10/12"
+		alt=""
+	/>
 </section>
 
-<section class="p-8">
-  <div class="p-4 border-2 border-lightgray rounded-lg">
-    <ApexCharts 
-      series={chartSeries} 
-      options={chartOptions}
-      height="700px"
-    />
-  </div>
+<section class="px-44 py-8">
+	<div class="p-4 border-2 border-lightgray rounded-lg">
+		<ApexCharts series={chartSeries} options={chartOptions} height="700px" />
+	</div>
 </section>
 
 <section class="text-blue-dark p-24 flex gap-8">
-  <div>
-    <h2 class="text-3xl font-bold">Services utilisés</h2>
-    <p class="pt-8 text-xl">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium blandit consectetur. Fusce sed accumsan metus, vitae fringilla augue. Duis tristique ligula vitae ante lacinia porta. Aliquam volutpat at nulla ut posuere. Vivamus semper eros et mauris tempor, vel pretium mauris commodo. Sed luctus sem lacus, a feugiat massa tempus ac
-    </p>
+	<div>
+		<h2 class="text-3xl font-bold">Services utilisés</h2>
+		<p class="pt-8 text-xl">
+			Les charges financières représentent les coûts liés à la gestion de la dette et des
+			financements, les intérêts, les frais bancaires et tout autre coût financier associé à la
+			gestion des ressources financières.
+		</p>
 
-    <p class="pt-8 text-xl">
-      Pellentesque tempus felis eu nisl condimentum, eget ornare mauris bibendum. Maecenas cursus dui id cursus gravida. Donec ante ex, vulputate a nunc vitae, sagittis ornare turpis. Donec viverra purus sed feugiat ornare. Duis id metus id quam rutrum laoreet eu at est.
-    </p>
-  </div>
+		<p class="pt-8 text-xl">
+			Les coûts généraux de fonctionnement et d'administration sont des dépenses nécessaires pour
+			assurer le bon fonctionnement de l'institution. Les dépenses de personnels couvrent les coûts
+			liés aux salaires, avantages sociaux et autres dépenses liées aux employés.
+		</p>
+		<p class="pt-8 text-xl">
+			Les dépenses FSRIF peuvent inclure des programmes spécifiques, des projets d'investissement,
+			ou d'autres initiatives. Les subventions désignent les fonds octroyés par le FSRIF à des
+			bénéficiaires externes, tels que des municipalités ou des organisme.
+		</p>
+		<p class="pt-8 text-xl">
+			La catégorie FCCT englobe les dépenses liées au Fonds de Compensation de la Taxe
+			Professionnelle tel que des ajustements budgétaires, des remboursements.
+		</p>
+		<p class="pt-8 text-xl">
+			Enfin, les autres dépenses peuvent inclure des coûts inattendus, des ajustements budgétaires
+			ou d'autres dépenses imprévues nécessitant une allocation spécifique.
+		</p>
+	</div>
 
-  <img src="./assets/expenses-services.svg" alt="">
+	<img src="./assets/expenses-services.svg" alt="" />
 </section>
