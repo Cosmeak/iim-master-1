c84c5093d43b911df92a82a4d25b6a9c573c9eb6
Maxime <maxime.britto@unify-technologies.com>
2024-02-16 10:01:02 +0100

    add point in the map to detect shop around

--- src/routes/commerces/+page.ts
+++ src/routes/commerces/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const response = await fetch(
-		`https://data.issy.com/api/explore/v2.1/catalog/datasets/touslescommercesdissy-les-moulineaux-feuille1/records?limit=100`
+		`https://data.issy.com/api/explore/v2.1/catalog/datasets/gpso_prop_secteur_encombrant/records?where=distance(geo_point_2d%2C%20geom'POINT(2.26667%2048.816669)'%2C%201km)&limit=100`
 	);
 	const data = await response.json();
 	return { data };
