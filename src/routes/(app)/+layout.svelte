3875e7edc83e1e37c83403cc76f8fc5ec778305d
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2024-06-26 20:56:51 +0200

    EP-8 wip: basic page with link to be

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,1 +1,29 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Card from '$lib/components/ui/card';
+	import { MessagesSquare, House, CalendarDays, LayoutList, BookOpen } from 'lucide-svelte';
+</script>
+
 <slot />
+
+<nav class="fixed bottom-0 left-0 right-0 m-2">
+	<Card.Root class="bg-cgreen p-4">
+		<Card.Content class="flex items-center justify-around p-0">
+			<Button href="/" class="rounded-full bg-white p-4">
+				<House size={24} color="#465A54" strokeWidth={1.5} />
+			</Button>
+			<Button href="/calendar" class="rounded-full bg-white p-4">
+				<CalendarDays size={24} color="#465A54" strokeWidth={1.5} />
+			</Button>
+			<Button href="/dialog" class="rounded-2xl bg-red-400 p-5">
+				<MessagesSquare size={24} color="#465A54" strokeWidth={2} />
+			</Button>
+			<Button href="/todo" class="rounded-full bg-white p-4">
+				<LayoutList size={24} color="#465A54" strokeWidth={1.5} />
+			</Button>
+			<Button href="/" class="rounded-full bg-white p-4">
+				<BookOpen size={24} color="#465A54" strokeWidth={1.5} />
+			</Button>
+		</Card.Content>
+	</Card.Root>
+</nav>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,1 +1,154 @@
-<h1>Welcome to Echo</h1>
+<script lang="ts">
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<div class="m-4 flex items-center justify-between">
+	<h1 class="text-2xl">Tâches du jour</h1>
+	<div class="h-12 w-12 rounded-full bg-black" />
+</div>
+
+<Card.Root class="bg-cgreen m-4 mt-6 pt-4 text-white">
+	<Card.Content class="text-center">
+		<div class="rounded-full border-4 border-white p-2">
+			<p class="text-4xl font-semibold">50%</p>
+			<p>des tâches d’aujourd’hui terminées</p>
+		</div>
+	</Card.Content>
+</Card.Root>
+
+<div class="m-4 mt-6">
+	<h2 class="text-2xl font-bold">Tâches en cours</h2>
+	<div class="flex w-full flex-col items-center">
+		<Card.Root class="mt-4 w-full">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+
+		<Card.Root class="mt-4 w-full">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+
+		<Card.Root class="mt-4 w-full">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+
+		<Card.Root class="mt-4 w-full">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+
+		<Card.Root class="mt-4 w-full">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+	</div>
+</div>
+
+<div class="m-4 mt-6">
+	<h2 class="text-2xl font-bold">Tâches terminées</h2>
+	<div class="flex w-full flex-col items-center">
+		<Card.Root class="mt-4 w-full border-2 border-green-500">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+
+		<Card.Root class="mt-4 w-full border-2 border-green-500">
+			<Card.Header class="flex flex-row items-center justify-between pb-1">
+				<p class="font-semibold">Lorem Ipsum</p>
+				<p class="text-sm text-muted-foreground">De 10h à 11h (1h)</p>
+			</Card.Header>
+			<Card.Content>
+				<p class="line-clamp-2">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+				</p>
+				<div class="mt-2">
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+					<Badge>Tag</Badge>
+				</div>
+			</Card.Content>
+		</Card.Root>
+	</div>
+</div>
